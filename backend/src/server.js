7c32dde846720df6d0efbffd984ea0ffa7e49ebe
Alexandre Kuns Giacobo <alexandrekgiacobo@gmail.com>
2020-11-04 20:25:40 -0300

    remove username and password

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,34 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const routes = require('./routes');
+const routes = require("./routes");
 
 const app = express();
-const server = require('http').Server(app)
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 const connectedUsers = {};
 
-io.on('connection', socket => {
-    const { user } = socket.handshake.query;
+io.on("connection", (socket) => {
+  const { user } = socket.handshake.query;
 
-    console.log(user, socket.id)
+  console.log(user, socket.id);
 
-    connectedUsers[user] = socket.id;
-})
+  connectedUsers[user] = socket.id;
+});
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0.eby9h.mongodb.net/omnistack8?retryWrites=true&w=majority', { useNewUrlParser: true});
+mongoose.connect(
+  "mongodb+srv://<username>:<password>@cluster0.eby9h.mongodb.net/<databasename>?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
 
 app.use((req, res, next) => {
-    req.io = io;
-    req.connectedUsers = connectedUsers;
+  req.io = io;
+  req.connectedUsers = connectedUsers;
 
-    return next();
+  return next();
 });
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use(routes);
 
 server.listen(3333);
